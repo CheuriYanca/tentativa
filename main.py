7af56b62342fbccccf47b3189143d68857658335
CheuriYanca <cheuri.ymacielm@gamil.com>
2025-03-08 22:22:03 -0400

    feat: backend cadastro

--- main.py
+++ main.py
@@ -12,14 +12,27 @@ Usuario = {
 def login():
     return render_template("login.html")
 
-@app_Cheuri.route("/login", methods = ["POST"])
-def autenticar():
-    email = request.from.get("email")
-    senha = request.from.get("passoword")
-    
-    if email == Usuario["email"] and senha == Usuario["senha"]:
-        flash("Lpgin realizado com sucesso!", "sucess")
-    else:
-        flash("Credenciais inválidas. Tente novamente.", "danger")
+@app_Cheuri.route("/cadastro", methods=["GET", "POST"])
+def cadastro():
+    if request.method == "POST":
+        nome_completo = request.form["name"]
+        cpf = request.form["cpf"]
+        email = request.form["email"]
+        
+        telefone = request.form["phone"]
+        endereco = request.form["address"]
+        senha = request.form["password"]
+        confirmar_senha = request.form["confirm_password"]
+
+        if senha != confirmar_senha:
+            flash("As senhas inseridas não coincidem. Tente novamente!", "error")
+            return redirect(url_for("cadastro"))
+        
+        flash("Cadastro efetuado com sucesso!", "success")
         return redirect(url_for("login"))
+
+    return render_template("cadastro.html")
+
+if __name__ == "__main__":
+    app_Cheuri.run(debug=True)
     
